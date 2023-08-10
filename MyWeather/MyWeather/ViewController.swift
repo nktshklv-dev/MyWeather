038f4c1ad520299c40f0733447966f03daa6d35a
Nikita Shakalov <nikitashakalov@gmail.com>
2023-08-10 17:52:46 +0300

    initial commit

---
+++ MyWeather/MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Nikita  on 8/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
