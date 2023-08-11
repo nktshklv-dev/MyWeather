18f17c9b89d6da34725b0a226834b7edff837a6c
Nikita Shakalov <nikitashakalov@gmail.com>
2023-08-12 01:57:47 +0300

    added WeatherModel and WeatherManager to manage API calls and get weather data

---
+++ MyWeather/Networking/APIManager.swift
@@ -0,0 +1,61 @@
+//
+//  APIManager.swift
+//  MyWeather
+//
+//  Created by Nikita  on 8/12/23.
+//
+
+import Foundation
+
+class WeatherManager {
+    
+    private let apiKey = "45b2ab5d9fad4f728a6213736231108"
+    
+    func getWeather(lat: Double, lon: Double) -> WeatherModel? {
+        guard let data = getWeatherData(lat: lat, lon: lon) else {return nil}
+        let weatherModel = decodeWeatherData(data: data)
+        return weatherModel
+    }
+    
+    func getWeather(cityName: String) -> WeatherModel? {
+        guard let data = getWeatherData(cityName: cityName) else {return nil}
+        let weatherModel = decodeWeatherData(data: data)
+        return weatherModel
+    }
+    
+    private func getWeatherData(lat: Double, lon: Double) -> Data? {
+        let urlString = "http://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(lat),\(lon)"
+        var weatherData: Data? = nil
+        do {
+            guard let url = URL(string: apiKey) else {return nil}
+            weatherData = try Data(contentsOf: url)
+        } catch {
+            print(error)
+        }
+        return weatherData
+    }
+    
+    private func getWeatherData(cityName: String) -> Data? {
+        let urlString = "http://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(cityName)"
+        var weatherData: Data? = nil
+        do {
+            guard let url = URL(string: apiKey) else {return nil}
+            weatherData = try Data(contentsOf: url)
+        } catch {
+            print(error)
+        }
+        return weatherData
+    }
+    
+    private func decodeWeatherData(data: Data) -> WeatherModel {
+        let decoder = JSONDecoder()
+        var weatherModel: WeatherModel!
+        do {
+            weatherModel = try decoder.decode(WeatherModel.self, from: data)
+        } catch {
+            print(error)
+        }
+        return weatherModel
+        
+    }
+}
---
+++ MyWeather/WeatherModel/WeatherModel.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherModel.swift
+//  MyWeather
+//
+//  Created by Nikita  on 8/12/23.
+//
+
+import Foundation
+
+struct WeatherModel: Codable {
+    var location: Location
+    var current: Current
+}
+
+struct Location: Codable {
+    var name: String
+    var region: String
+    var country: String
+    var lat: Double
+    var lon: Double
+    var localtime: String
+}
+
+struct Current: Codable {
+    var temp_c: Double
+    var temp_f: Double
+    var condition: Condition
+    var wind_mph: Double
+    var wind_kph: Double
+    var wind_degree: Int
+    var wind_dir: String
+    var pressure_mb: Int
+    var pressure_in: Double
+    var precip_mm: Double
+    var precip_in: Double
+    var humidity: Int
+    var cloud: Int
+    var feelslike_c: Double
+    var feelslike_f: Double
+    var vis_km: Int
+    var vis_miles: Int
+    var uv: Int
+    var gust_mph: Double
+    var gust_kph: Double
+}
+
+struct Condition: Codable {
+    var text: String
+}
