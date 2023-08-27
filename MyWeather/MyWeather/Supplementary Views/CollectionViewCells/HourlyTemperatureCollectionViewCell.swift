f1ce194197f262297c3f8178a5df01b0775adc5f
Nikita Shakalov <nikitashakalov@gmail.com>
2023-08-28 02:40:25 +0300

    created time label, condition icon, temp label for collection view cells

--- MyWeather/MyWeather/Supplementary Views/CollectionViewCells/HourlyTemperatureCollectionViewCell.swift
+++ MyWeather/MyWeather/Supplementary Views/CollectionViewCells/HourlyTemperatureCollectionViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 class HourlyTemperatureCollectionViewCell: UICollectionViewCell {
     
     static let reuseIdentifier = "HourlyTemperatureCollectionViewCell"
+    
     private var currentTimeLabel: UILabel!
     private var currentConditionIcon: UIImageView!
     private var currentTemperatureLabel: UILabel!
@@ -26,11 +27,43 @@ class HourlyTemperatureCollectionViewCell: UICollectionViewCell {
     
     private func setupViews() {
         self.backgroundColor = R.color.gray()
-        self.layer.cornerRadius = 8 
+        self.layer.cornerRadius = 8
+        
+        currentTimeLabel = UILabel()
+        currentTimeLabel.text = "22:00"
+        currentTimeLabel.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        currentTimeLabel.textColor = R.color.labelGray()
+        self.contentView.addSubview(currentTimeLabel)
+        
+        currentConditionIcon = UIImageView()
+        currentConditionIcon.image = UIImage(systemName: "moon.fill")
+        currentConditionIcon.tintColor = R.color.labelGray()
+        self.contentView.addSubview(currentConditionIcon)
+        
+        currentTemperatureLabel = UILabel()
+        currentTemperatureLabel.text = "20°C"
+        currentTemperatureLabel.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        currentTemperatureLabel.textColor = R.color.labelGray()
+        self.contentView.addSubview(currentTemperatureLabel)
     }
     
     private func setupConstraints() {
+        currentTimeLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.contentView.snp.top).offset(16)
+            make.centerX.equalToSuperview()
+        }
         
+        currentConditionIcon.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(32)
+            make.width.equalTo(30)
+            make.top.equalTo(currentTimeLabel.snp.bottom).offset(8)
+        }
+        
+        currentTemperatureLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(self.contentView.snp.bottom).offset(-16)
+            make.centerX.equalToSuperview()
+        }
     }
     
     
